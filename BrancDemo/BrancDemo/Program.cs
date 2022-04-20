c86ea0731ab28ae43be2f55e44ec923323d9151b
Tommy Waalkes <Tommy@grandcircus.co>
2022-04-20 13:44:17 -0400

    Creating conflict

--- BrancDemo/BrancDemo/Program.cs
+++ BrancDemo/BrancDemo/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Salami is great!!!!");
         Console.WriteLine("Burger King is my King!");
     }
 }
